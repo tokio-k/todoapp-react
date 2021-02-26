de946f58a138885bb1968c37511528ebfb8d2c12
Toki <55911995+tokio-k@users.noreply.github.com>
2021-02-26 15:05:16 +0900

    create TodoApp

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import createStore from './createStore';
+
+const store = createStore();
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
